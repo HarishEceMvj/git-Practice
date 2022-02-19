235ea00397fac91066a5ba9132a194ec575d8f61
Harish <harishpv.ece@outlook.com>
2022-02-19 15:57:07 +0530

    updated calculator.py file to perform addition and subtraction

--- calc_addition_two_num.py
+++ calc_addition_two_num.py
@@ -1,3 +1,16 @@
-a = int(input("enter the number"))
-b = int(input("enter the number"))
-print("sume is ", a+b)
+while True:
+    print("this script perform only additon and subtraction")
+    selection = input("enter the selection")
+        
+
+    if selection == addition:
+        a = int(input("enter the number"))
+        b = int(input("enter the number"))
+        print("sum is ", a+b)
+    elif selection == subtraction:
+        a = int(input("enter the number"))
+        b = int(input("enter the number"))
+
+        print("subtraction is ", a-b)
+    else:
+        print("wrong selection')
