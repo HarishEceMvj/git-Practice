7baf2121fe6c374dd48e384d6a83747a18cfbc6a
Harish <harishpv.ece@outlook.com>
2022-02-20 19:06:24 +0530

    updated add in calc.py file

--- calc_addition_two_num.py
+++ calc_addition_two_num.py
@@ -1,9 +1,9 @@
 while True:
-    print("this script perform only additon and subtraction")
+    print("this script perform only add and subtraction")
     selection = input("enter the selection")
         
 
-    if selection == addition:
+    if selection == add:
         a = int(input("enter the number"))
         b = int(input("enter the number"))
         print("sum is ", a+b)
