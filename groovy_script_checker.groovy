b9281eec8bc076c7dcbeafc5207e4dd0f775af93
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-09-07 14:13:41 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -1,29 +1,11 @@
 pipeline {
     agent any
-    stages {
-        stage ('groovy_script_checker') {
-        try {
-            prepareVSASANDeployCCStage("vg_smoke_san").call()
-   
-        } catch (e) {
-            // If there was an exception thrown, the build failed
-            currentBuild.result = "FAILED"
-            throw e
-        } finally {
-            // Success or failure, always send notifications
-			}
-		}
-	}
-}
-	def prepareVSASANDeployCCStage(testSetName) {
-  sh('''
-          case "${testSetName}" in
-             "vsa_smoke_san") echo "./deploy_vsa -d $VTRIDENT_DEPLOY -m block -p ${VSA_PASSWORD} ${VSA_SAN_APPLIANCE_NAMES} VSA"
-                ;;
-             "vg_smoke_san") echo "./deploy_vsa -d $VTRIDENT_DEPLOY -m block -p ${VSA_PASSWORD} ${VG_APPLIANCE_NAMES} VGA"
-                ;;
-          esac
-        sleep 900
-  ''')
-  }
 
+    stages {
+        stage('Hello') {
+            steps {
+                sh '''python3 
+            }
+        }
+    }
+}
