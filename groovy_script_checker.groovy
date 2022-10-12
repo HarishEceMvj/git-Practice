b10659c344e652e21cfc7ce0bcf057ad98877254
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-12 19:18:50 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -6,7 +6,13 @@ pipeline {
             steps {
                 
                 sh "python3 jenkins_build_failure_manually_checker.py -n $number"
-            } 
+                 } 
+            }
+        stage('finish') {
+            steps {
+            
+                sh 'echo BUILD_status = $CHECK_ENV'
+            }
         }
     }
 }
