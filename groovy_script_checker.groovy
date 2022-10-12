19a46683be2784b0b752c4b90fbb83dc759d4851
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-12 16:25:43 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -5,11 +5,6 @@ pipeline {
         stage('build_status_checker') {
             steps {
                 
-                sh '''#!/bin/bash
-                python3 jenkins_build_failure_manually_checker.py -n $number
-                echo ${env.BUILD_NUMBER}
-                echo ${env.CHECK_ENV}
-                '''
                 sh 'echo ${env.BUILD_NUMBER}'
             } 
         }
