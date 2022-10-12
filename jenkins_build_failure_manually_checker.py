280c941d0f3958989bcfbd2b8a94d89998a12378
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-12 16:18:19 +0530

    Update jenkins_build_failure_manually_checker.py

--- jenkins_build_failure_manually_checker.py
+++ jenkins_build_failure_manually_checker.py
@@ -5,10 +5,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-n', dest="value", help='provide number to check the devide by zero error')
 
 args = parser.parse_args()
-
+os.environ['CHECK_ENV'] = "PASS"
 try:
     out = 10 / int(args.value)
     print(f'output is {out}')
 except Exception as e:
     print(f"error observed is : {e}")
-    os.environ['check_env'] = "fail"
+    os.environ['CHECK_ENV'] = "FAIL"
