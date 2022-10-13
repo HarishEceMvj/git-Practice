c34d7da0519f93a2e09a75f3dd22027830360594
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-13 10:12:16 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -1,19 +1,6 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('build_status_checker') {
-            steps {
-                
-                def result = sh (returnStatus: true, script: 'python3 jenkins_build_failure_manually_checker.py -n $number').trim()
-                println("current build status: "+ result)
-                 } 
-            }
-        stage('finish') {
-            steps {
-            
-                sh 'echo BUILD_status = $CHECK_ENV'
-            }
-        }
+node {
+    stage('Stage') {
+        def result = sh returnStatus: true, script: 'python3 jenkins_build_failure_manually_checker.py -n $number'
+        echo "RESULT IS $result"
     }
 }
