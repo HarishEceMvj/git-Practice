90a69d185906ae5fd124597394b696179e740395
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-13 17:05:28 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -3,7 +3,8 @@ node {
         environment {
         RESULT = sh (returnStdout: true, script: 'python3 jenkins_build_failure_manually_checker.py -n $number').trim()
         }
+        steps {
         echo "${env.RESULT}"
-        
+        }
     }
 }
