685251b6fa307aa0790360e2657fd49c13e57470
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-13 17:14:32 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -2,7 +2,7 @@ node {
     stage('Stage') {
         
        def result = sh (returnStdout: true, script: 'python3 jenkins_build_failure_manually_checker.py -n $number').trim()
-       println("current build status: "+ result)
+       println(result)
         
     }
 }
