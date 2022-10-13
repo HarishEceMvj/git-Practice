00f5febfae7df08776df0eccaebdd040ed3254cc
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-10-13 15:14:45 +0530

    Update groovy_script_checker.groovy

--- groovy_script_checker.groovy
+++ groovy_script_checker.groovy
@@ -2,7 +2,7 @@ node {
     stage('Stage') {
         def result = sh (returnStdout: true, script: 'python3 jenkins_build_failure_manually_checker.py -n $number').trim()
         echo "RESULT IS $result"
-        if ($result == 'ERROR') {
+        if (result == 'ERROR') {
             currentBuild.currentResult = "FAILURE"
         }
     }
